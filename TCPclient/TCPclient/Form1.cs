e38a5d95e9235f0f116e1ebb6acf28620cd9d58c
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-16 10:32:59 +0900

    TCP 서버/클라이언트 서버연결후 이미지 불러오기
    서버에서 이미지를 Picture 박스에 담은 뒤 소켓 연결 후
    
    Client가 서버에 있는 이미지를 받아온다.

---
+++ TCPclient/TCPclient/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e) // 서버와 연결 후 이미지를 가져오는 버튼
+        {
+            Socket sClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("10.40.10.69"), 9999);
+            sClient.Connect(ipEndPoint);
+            Byte[] _data = new byte[1024];
+            sClient.Receive(_data); // 클라이언트에서 데이터 수신
+            int iLength = BitConverter.ToInt32(_data, 0);
+
+            Byte[] _data2 = new byte[iLength];
+            sClient.Receive(_data2);
+
+            this.pictureBox1.Image = byteArrayToImage(_data2);
+
+            sClient.Close();
+        }
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
---
+++ TCPserver/TCPserver/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 이미지를 가져오는 버튼
+        {
+            OpenFileDialog dia = new OpenFileDialog();
+            dia.Multiselect = false;
+            dia.Filter = "|*.jpg"; // 이미지 파일 타입은 .jpg 파일
+
+            if (dia.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox1.ImageLocation = dia.FileName; // 이미지를 가져올시 픽쳐박스에 생성
+            }
+        }
+        public byte[] ImageToByteArray(System.Drawing.Image image)
+        {
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray(); // 이미지를 배열에 반환한다.
+        }
+
+        private void button2_Click(object sender, EventArgs e) // 클라이언트와의 연결 대기하는 버튼
+        {
+            this.button2.Enabled = false;
+
+            Socket sListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 9999);
+
+            sListener.Bind(ipEndPoint);
+            sListener.Listen(20);
+
+            Console.WriteLine("클라이언트 연결을 대기합니다.");
+
+            Socket sClient = sListener.Accept(); //소켓에서 요청이 올때까지 대기
+            IPEndPoint ip = (IPEndPoint)sClient.RemoteEndPoint;
+            Console.WriteLine("주소 {0} 에서 접속", ip.Address);
+
+            Byte[] _data = ImageToByteArray(this.pictureBox1.Image);
+            sClient.Send(BitConverter.GetBytes(_data.Length));
+            sClient.Send(_data);
+
+            sListener.Close();
+        }
+    }
+}
