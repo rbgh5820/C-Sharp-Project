1abe11a785ba34a1d17451ed845f2306d92a0047
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-16 10:21:17 +0900

    C Sharp 버튼으로 DB정보 추가하기
    C# WINDOWS Forms 프로젝트에서 버튼으로 DB베이스에 연결된 정보를 추가하는 기능

---
+++ DB insert Form/DB insert Form/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace DB_insert_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbDBinsert = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(446, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DB 추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbDBinsert
+            // 
+            this.lbDBinsert.AutoSize = true;
+            this.lbDBinsert.Location = new System.Drawing.Point(286, 282);
+            this.lbDBinsert.Name = "lbDBinsert";
+            this.lbDBinsert.Size = new System.Drawing.Size(60, 25);
+            this.lbDBinsert.TabIndex = 1;
+            this.lbDBinsert.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbDBinsert);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbDBinsert;
+    }
+}
+
---
+++ DB insert Form/DB insert Form/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using log4net;
+using log4net.Config;
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml")]
+
+namespace DB_insert_Form
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Form1));
+
+        public Form1()
+        {
+            InitializeComponent();
+            XmlConfigurator.Configure(new System.IO.FileInfo("log4net"));
+        }
+        public void MariaDB_insert()
+        {
+            string connstr = "Server=10.40.10.69; Port=3306; Database=test; Uid=root; Pwd=12431243a";
+            MySqlConnection conn = new MySqlConnection(connstr);
+            MySqlCommand cmd = conn.CreateCommand();
+            string sql = "insert into movie(movie_name,director,types) values('영화1','감독4','???2')";
+            cmd.CommandText = sql;
+            try
+            {
+                conn.Open();
+            }catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            MySqlDataReader reader = cmd.ExecuteReader();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MariaDB_insert();
+            lbDBinsert.Text=("DB가 추가 되었습니다");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.Debug("Form1 Loaded Complete~!");
+        }
+    }
+}
