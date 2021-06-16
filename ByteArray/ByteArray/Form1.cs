be5752827ac6d6cb6ba7a98f0b9d7d0fcac7dc74
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-16 17:29:40 +0900

    TCP 방식 서버/클라이언트 파일저장 및 클라이언트에 폼 이미지 띄우기
    서버에서 소켓연결 대기를하고 클라이언트가 서버에있는 이미지를 가져온다
    ( 원래는 순서 반대로 )

---
+++ ByteArray/ByteArray/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+using log4net.Config;
+using System.Net.Sockets;
+using System.Net;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net")]
+
+
+namespace ByteArray
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Form1));
+
+        public Form1()
+        {
+            InitializeComponent();
+            XmlConfigurator.Configure(new System.IO.FileInfo("log4net.xml"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //이미지를 바이트 배열로 변환
+            Image img = Image.FromFile("C://test//600.jpg");
+            byte[] bArr = imgToByteArray(img);
+
+            //바이트 배열을 이미지로 전환
+            Image img1 = byteArrayToImage(bArr);
+            pictureBox1.Image = img;
+        }
+
+        private Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            using (MemoryStream mStream = new MemoryStream(byteArrayIn))
+            {
+                return Image.FromStream(mStream);
+            }
+        }
+
+        private byte[] imgToByteArray(Image img)
+        {
+            using (MemoryStream mStream = new MemoryStream())
+            {
+                img.Save(mStream, img.RawFormat);
+                return mStream.ToArray();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Title = "다른 이름으로 저장";
+            dlg.DefaultExt = "jpg";
+            dlg.Filter = "JPEG (*.jpg)|*.jpg";
+            dlg.FilterIndex = 0;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(dlg.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.button2.Enabled = false;
+
+            Socket sListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 9999);
+
+            sListener.Bind(ipEndPoint); // 주소를 할당한다.
+            sListener.Listen(20);
+
+            Socket sClient = sListener.Accept(); //소켓에서 요청이 올때까지 대기
+            IPEndPoint ip = (IPEndPoint)sClient.RemoteEndPoint;
+            Console.WriteLine("주소 {0} 에서 접속", ip.Address);
+
+            Byte[] _data = imgToByteArray(this.pictureBox1.Image);
+            sClient.Send(BitConverter.GetBytes(_data.Length));
+
+            sClient.Send(_data);
+
+            sListener.Close();
+        }
+    }
+}
---
+++ ByteArray2/ByteArray2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ByteArray2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket sClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);
+            sClient.Connect(ipEndPoint);
+            Byte[] _data = new byte[500];
+            
+            int nsize = sClient.Receive(_data); // Receive의 리턴값을 nsize에 반환
+            Byte[] size = new byte[nsize];
+            Buffer.BlockCopy(_data, 0, size, 0, nsize); // _data
+            int iLength = BitConverter.ToInt32(size, 0);
+
+            Byte[] _data2 = new byte[iLength];
+            sClient.Receive(_data2);
+            this.pictureBox1.Image = byteArrayToImage(_data2);
+            sClient.Close();
+        }
+        private Image byteArrayToImage(byte[] data2)
+        {
+            MemoryStream ms = new MemoryStream(data2);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
