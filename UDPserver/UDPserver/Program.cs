bfa448ce70d1c37db678372e3de6ce788e2a82b7
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-16 10:25:33 +0900

    UDP 서버/클라이언트 메시지 보내기

---
+++ UDPserver/UDPserver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace UDPserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1000*1000];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 8888);
+            UdpClient newsock = new UdpClient(ipep);
+
+            Console.WriteLine("수신 대기중...");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            data = newsock.Receive(ref sender);
+
+            Console.WriteLine("{0}에서 수신된 메세지", sender.ToString());
+            Console.WriteLine(Encoding.Unicode.GetString(data, 0, data.Length));
+
+            string welcome = "welcome to my test server";
+            data = Encoding.Unicode.GetBytes(welcome);
+            newsock.Send(data, data.Length, sender);
+
+            while (true)
+            {
+                data = newsock.Receive(ref sender);
+
+                Console.WriteLine("{0}에서 수신된 메세지", sender.ToString());
+                Console.WriteLine(Encoding.Unicode.GetString(data, 0, data.Length));
+                newsock.Send(data, data.Length, sender);
+            }
+        }
+    }
+}
