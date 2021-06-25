7081832d25cbaaeddfdc20e458eab3dc45c06f8b
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-25 18:28:57 +0900

    Ipl 이미지 파일로 저장한거 불러오기

---
+++ 캠 불러오기.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 캠_불러오기
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = File.OpenRead(@"C://test//byte2Test");
+
+            byte[] data = new byte[fs.Length];
+
+            fs.Read(data, 0, data.Length);
+            fs.Close();
+
+            this.pictureBoxIpl1.Image = byteArrayToImage(data);
+        }
+
+        private Image byteArrayToImage(byte[] data)
+        {
+            MemoryStream ms = new MemoryStream(data);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
