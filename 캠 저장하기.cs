b1b797b4ec92dd7264064c0bb071598384ad9fa1
rbgh5820 <82484101+rbgh5820@users.noreply.github.com>
2021-06-28 11:48:52 +0900

    pictureBoxIpl에 있는 이미지를 저장하는 코드

--- 캠 저장하기.cs
+++ 캠 저장하기.cs
@@ -26,8 +26,8 @@ namespace 캠_저장하기
         IplImage src;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            src = capture.QueryFrame();
-            pictureBoxIpl1.ImageIpl = src;
+            src = capture.QueryFrame(); //src에 frame을 받아온다.
+            pictureBoxIpl1.ImageIpl = src; // picturboxipl박스에 src 영상을 출력
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,8 +35,8 @@ namespace 캠_저장하기
             try
             {
                 capture = CvCapture.FromCamera(CaptureDevice.DShow, 0);
-                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 900);
-                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 700);
+                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 900); // 영상 넓이
+                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 700); // 영상 높이
             }
             catch
             {
@@ -46,15 +46,18 @@ namespace 캠_저장하기
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Cv.ReleaseImage(src);
+            //폼을 닫으면서 메모리 할당 해제
+            Cv.ReleaseImage(src); // 이미지의 메모리 할당 해제
             if (src != null) src.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Image captureImage = ConvertToBitmap(pictureBoxIpl1.ImageIpl); // 비트맵으로 전환
-            Image dataImage = new Bitmap(captureImage); // bitmap으로 전환된 captureImage를 dataImage에 저장
+            Image captureImage = ConvertToBitmap(pictureBoxIpl1.ImageIpl); // pictureboxIpl에있는 이미지를 비트맵으로 전환하는 객체 선언
+            Image dataImage = new Bitmap(captureImage); // bitmap으로 전환된 captureImage를 dataImage에 저장하는 객체 선언
             byte[] data = imageToBytes(dataImage); // dataImage를 배열에 저장
+            
+            //FileMode.Create : 파일을 생성할 때, FileMode.Open : 파일 읽을 때, FileAccess.Write(Read) : 파일의 액세스 권한
             FileStream fs = new FileStream("C:\\test\\byte2Test", FileMode.Create, FileAccess.Write);
 
             fs.Write(data, 0, data.Length);
